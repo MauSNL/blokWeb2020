b86474b28fb268d5a4e8c3641e45d2cd95fc95ee
Mauro Stuifzand <maurostuif@hotmail.com>
2020-10-05 17:58:29 +0200

    Hamburger zonder link transitions
    Ik heb een hamburger menu toegevoegd aan de Nav. Deze is voorzien van kleur transities en een groei van de navigatie balk die aan de hand van overflow de navigatie links toont. Ik moet nog een transitie toevoegen van link op de linkjes zelf en de social kanalen moeten er nog in.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+const span = document.querySelector('span');
+const menu = document.querySelector('header')
+const button = document.querySelector('.click')
+const logo = document.querySelector('.logo')
+let imgUrl = ["images/logo.png", "images/logo_alt.png"]
+
+button.addEventListener('click', function(){
+  span.classList.toggle('active');
+  menu.classList.toggle('menu');
+
+  if(logo.getAttribute("src") ===imgUrl[0]){
+  logo.setAttribute("src",imgUrl[1])
+  }else{
+
+  logo.setAttribute("src",imgUrl[0])
+  }
+}, false);
